1e5ec13015ff103eaff8cdf0c195fcd2d325ffbb
Cassius Villareal <cassiusfive@gmail.com>
2023-12-23 19:28:34 -0800

    Fixed Scraping Logic

--- backend/src/scraping/registrationScraper.ts
+++ backend/src/scraping/registrationScraper.ts
@@ -2,38 +2,51 @@ import axios, {AxiosInstance} from "axios";
 import { Catalog, Course, Offering, SectionType, Section } from "../shared.types";
 
 export default class RegistrationScraper {
-    private axiosInstance = axios.create({ withCredentials: true });
+    private axiosInstance = axios.create({
+        baseURL: 'https://prodapps.isadm.oregonstate.edu/StudentRegistrationSsb/ssb/',
+        withCredentials: true,
+    });
 
     public async scrapeCourse(title: string): Promise<Course> {
-        await axios.post('https://prodapps.isadm.oregonstate.edu/StudentRegistrationSsb/ssb/term/search?mode=search', { term: 202402 }, {
-            headers: {'content-type': 'application/x-www-form-urlencoded'}
+        const setTerm = await this.axiosInstance.post('term/search?mode=search', { term: 202402 }, {
+            headers: {'content-type': 'application/x-www-form-urlencoded'},
         });
 
-        const res = await this.axiosInstance.get('https://prodapps.isadm.oregonstate.edu/StudentRegistrationSsb/ssb/searchResults/searchResults', {
+        const cookies = setTerm.headers['set-cookie']?.join('; ') || '';
+
+        const courseSearch = await this.axiosInstance.get('searchResults/searchResults', {
             params: {
                 txt_campus: 'C',
                 txt_term: 202402,
                 txt_courseTitle: title,
                 pageOffset: 0,
                 pageMaxSize: 500,
+            },
+            headers: {
+                Cookie: cookies
             }
         });
 
-        return this.normalizeCourseData(res.data.data);
+        return this.normalizeCourseData(courseSearch.data.data);
     }
 
     private normalizeCourseData(data: any): Course {
-        data.filter((rawSection: any) => {
-            if (rawSection?.faculty != 1) {
+        if (!data) { throw new Error("No data provided") }
+        data = data.filter((rawSection: any) => {
+            const excludedAttributes = ['HNRS'];
+            if (rawSection?.faculty.length != 1) {
                 return false;
             }
             if (rawSection?.meetingsFaculty.length != 1 ) {
                 return false;
             }
+            if (rawSection.sectionAttributes?.some((attribute: any) => excludedAttributes.includes(attribute.code))) {
+                return false;
+            }
             return true;
         });
 
-        if (!data) { throw new Error("No data provided") }
+        if (data.length == 0) { throw new Error("Course data not normalized") }
 
         const course: Course = {
             title: data[0].courseTitle,
@@ -44,27 +57,27 @@ export default class RegistrationScraper {
         const linkedGroups: Record<string, Record<string, Section[]>> = {};
 
         for (const section of data) {
-            const linker = this.extractLink(section?.linkIdentifier) || 'Z'
+            const linker = this.extractLink(section?.linkIdentifier);
             if (!linkedGroups[linker]) {
                 linkedGroups[linker] = {};
             }
-            if (!linkedGroups[linker][section.meetingScheduleType]) {
-                linkedGroups[linker][section.meetingScheduleType] = [];
+            if (!linkedGroups[linker][section.scheduleTypeDescription]) {
+                linkedGroups[linker][section.scheduleTypeDescription] = [];
             }
-            linkedGroups[linker][section.meetingScheduleType].push({
+            linkedGroups[linker][section.scheduleTypeDescription].push({
                 crn: section.courseReferenceNumber,
                 credits: section.creditHours,
                 maxEnrollment: section.maximumEnrollment,
                 enrollment: section.enrollment,
                 instructorName: section.faculty[0].displayName,
                 instructorEmail: section.faculty[0].emailAddress,
-                start: this.normalizeTime(section.meetingsFaculty[0].beginTime),
-                end: this.normalizeTime(section.meetingsFaculty[0].endTime),
-                isMondayIncluded: section.meetingsFaculty[0].monday,
-                isTuesdayIncluded: section.meetingsFaculty[0].tuesday,
-                isThursdayIncluded: section.meetingsFaculty[0].thursday,
-                isWednesdayIncluded: section.meetingsFaculty[0].wednesday,
-                isFridayIncluded: section.meetingsFaculty[0].friday
+                start: this.normalizeTime(section.meetingsFaculty[0].meetingTime.beginTime),
+                end: this.normalizeTime(section.meetingsFaculty[0].meetingTime.endTime),
+                isMondayIncluded: section.meetingsFaculty[0].meetingTime.monday,
+                isTuesdayIncluded: section.meetingsFaculty[0].meetingTime.tuesday,
+                isThursdayIncluded: section.meetingsFaculty[0].meetingTime.thursday,
+                isWednesdayIncluded: section.meetingsFaculty[0].meetingTime.wednesday,
+                isFridayIncluded: section.meetingsFaculty[0].meetingTime.friday
             })
         }
 
@@ -82,7 +95,8 @@ export default class RegistrationScraper {
         return course;
     }
 
-    private extractLink(linkIdentifier: string) {
+    private extractLink(linkIdentifier: string | undefined) {
+        if (!linkIdentifier) {return 'NONE'}
         return linkIdentifier.replace(/[^a-zA-Z]/g, '');
     }
 
