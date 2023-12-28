59ff07b195a01fdd281cf0dfb858683e634d982a
Cassius Villareal <cassiusfive@gmail.com>
2023-12-28 02:52:05 -0800

    WeeklyCalendar in progress

---
+++ client/src/SectionEvent.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Section } from "./shared.types";
+
+interface SectionEventProps {
+	courseTitle: string,
+	sectionType: string,
+	section: Section
+}
+
+export const SectionEvent = ({courseTitle, sectionType, section}: SectionEventProps) => {
+
+}
