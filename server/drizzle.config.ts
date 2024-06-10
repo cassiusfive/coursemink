dfb4524d222665cdb1e0b85e55bdf8d9c7841943
Cassius Villareal <cassiusfive@gmail.com>
2024-06-10 00:40:19 -0700

    feat: drizzle migration

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -4,4 +4,7 @@ export default defineConfig({
   dialect: "postgresql",
   schema: "./src/db/schema.ts",
   out: "./drizzle",
+  dbCredentials: {
+    url: process.env.DATBASE_URL as string,
+  },
 });
